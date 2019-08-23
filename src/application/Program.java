72b6fac4fae4c90978595ff64b829724a58f371a
Celina Maximiano <git config --global user.name Celina>
2019-08-22 23:11:53 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -20,6 +21,7 @@ public class Program {
 		
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
@@ -28,6 +30,12 @@ public class Program {
 		System.out.println("\n=== TEST 2: seller findByDepartment ===");		
 		Department department = new Department(2, null);		
 		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}	
+		
+		System.out.println("\n=== TEST 3: seller findByDepartment ===");				
+		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}		
